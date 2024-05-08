a8e87749e33afc525baacceabc28033fbc7e28b2
Trey Turner <trey.turner1857@gmail.com>
2024-05-07 22:40:24 -0500

    changes the i variable to list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    i = set(my_list)
+    list = set(my_list)
     sum = 0
-    for num in my_list:
+    for num in list:
         sum += num
     return sum
