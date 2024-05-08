c38440381b1ea53216fc91da0dfc7927550fd0ea
Trey Turner <trey.turner1857@gmail.com>
2024-05-07 19:24:22 -0500

    in the if c == 0 print statement i put "o" instead of "0"

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     c = len(sys.argv) - 1
     if c == 0:
-        print("o arguments.")
+        print("0 arguments.")
     elif c == 1:
         print("1 argument:")
     else:
