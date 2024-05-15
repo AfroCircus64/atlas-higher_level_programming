0ec48bb68775d2144df6f8b7f858e0f135413523
Trey Turner <trey.turner1857@gmail.com>
2024-05-14 23:54:37 -0500

    created a readme and finished task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class rectangle"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self):
+        """Empty func to create a rectangle"""
+        pass
