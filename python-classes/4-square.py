e98658ad6b7570d8b82445216cdbc435148c2518
Trey Turner <trey.turner1857@gmail.com>
2024-05-14 21:42:10 -0500

    Added the Getter and Setter methods to the base code of task 3. Task 4 complete

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines the class square"""
+
+
+class Square:
+    """Class to define a square"""
+    def __init__(self, size=0):
+        """Creates the square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method"""
+        if type(value)!= int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area of the square"""
+        return self.__size ** 2
