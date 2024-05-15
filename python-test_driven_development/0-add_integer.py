f1afbb137b10bb5c3d8b6f33002a86161121b28b
Trey Turner <trey.turner1857@gmail.com>
2024-05-14 22:58:46 -0500

    redid the add_integer func

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,10 +2,12 @@
 """Defines the class add_integer"""
 def add_integer(a, b=98):
     """Class to add two integers"""
-    try:
-        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-            return int(a) + int(b)
-        else:
-            raise TypeError("a must be an integer or b must be an integer")
-    except ValueError:
-        raise TypeError("a must be an integer or b must be an integer")
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    elif type(a) is float:
+        a = int(a)
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    elif type(b) is float:
+        b = int(b)
+    return a + b
