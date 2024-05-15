84f095651e2ca04ab51240081231561721da9714
Trey Turner <trey.turner1857@gmail.com>
2024-05-14 21:11:21 -0500

    forgot to make 2-square.py (SMH) and kept coding in 1-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the class square"""
+
+
+class Square:
+    """Class to define a square"""
+    def __init__(self, size=0):
+        """Creates the square"""
+        self.__size = size
+
+        if type(size)!= int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
