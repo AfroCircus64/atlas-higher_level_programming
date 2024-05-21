d2360ef37e2d3bce853d67f484422ef715726164
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 16:14:07 -0500

    completed task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function to check if it's an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """Function to check for an instance"""
+
+    return type(obj) is a_class
