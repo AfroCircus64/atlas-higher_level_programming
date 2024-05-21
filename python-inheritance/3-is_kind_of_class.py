5e31c22399e43e048fc757992f4ee2626aa3a0e9
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 16:17:39 -0500

    completed task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a func that checks for instance from class or inherited"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to check for instance"""
+
+    return isinstance(obj, a_class)
