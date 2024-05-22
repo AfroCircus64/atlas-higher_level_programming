9ffc70f8cbfae40a01c329e3ed8c16a0c08ddcb6
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 23:59:04 -0500

    Completed task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines class Student"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes a student"""
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+
+        return vars(self)
