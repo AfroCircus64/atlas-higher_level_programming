909553e2f36fd48a8926ee3541bf45567a6172c5
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 22:51:37 -0500

    Fixed the initialization of class rectangle. had BaseException instead of BaseGeometry

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseException):
+class Rectangle(BaseGeometry):
     """Defines a class called Rectangle"""
 
     def __init__(self, width, height):
