2aa9073d4f8139ad61d7238030dbf04251aad919
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 21:57:09 -0500

    completed task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class called BaseGeometry"""
+
+
+class BaseGeometry():
+    """Empty class"""
+
+    def area(self):
+        """public instance method"""
+        raise Exception("area() is not implemented")
