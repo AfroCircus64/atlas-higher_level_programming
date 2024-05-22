d1255a1539c5159e140bdb7b9142700de1061f37
Trey Turner <trey.turner1857@gmail.com>
2024-05-21 22:35:21 -0500

    Completed task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseException):
+    """Defines a class called Rectangle"""
+
+    def __init__(self, width, height):
+        """Initialize the Rectangle"""
+
+        self._Rectangle__width = self.integer_validator("width", width)
+        self._Rectangle__height = self.integer_validator("height", height)
