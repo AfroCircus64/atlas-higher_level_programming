000a2874b5ae83d752532407d1b3cfd0493c88d1
Trey Turner <trey.turner1857@gmail.com>
2024-07-30 01:40:35 -0500

    Completed task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (err, response, body) {
+  if (err) throw err;
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed && task.userId) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
+      }
+    }
+  }
+  console.log(users);
+});
